b7ac0e8af16c06dd04c035f7589657e8fbb142ba
Maddi <maddi.fitzmaurice@gmail.com>
2023-06-29 16:19:06 +1000

    Reset
    - Starting afresh

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,62 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Data
-    public float MoveForce;
-    public float TargetVelocity;
-    public float JumpIdleForce;
-    public float JumpMoveForce;
-
-    // Transform Info
-    [HideInInspector] public Vector3 MoveDir;
-    public Transform CamDir;
-    
-    // Components
-    [HideInInspector] public Rigidbody Rb;
-    [HideInInspector] public BoxCollider Collider;
-    public Transform CastPos;
-
-    // State Machine
-    private StateMachine _stateMachine;
-    public PlayerIdleState IdleState;
-    public PlayerMoveState MoveState;
-    public PlayerJumpIdleState JumpIdleState;
-    public PlayerJumpMoveState JumpMoveState;
-
-    void Awake()
-    {
-        // State Machine
-        IdleState = new PlayerIdleState(this);
-        MoveState = new PlayerMoveState(this);
-        JumpIdleState = new PlayerJumpIdleState(this);
-        JumpMoveState = new PlayerJumpMoveState(this);
-        _stateMachine = new StateMachine();
-    }
-
+    // Start is called before the first frame update
     void Start()
     {
-        // Components
-        Rb = GetComponent<Rigidbody>();
-        Collider = GetComponent<BoxCollider>();
-
-        // Start in idle state
-        _stateMachine.ChangeState(IdleState);
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
         
-        _stateMachine.CurrentState.LogicUpdate();
     }
 
-    void FixedUpdate()
-    {
-        
-        _stateMachine.CurrentState.PhysicsUpdate();
-    }
-
-    public void ChangeState(BaseState newState)
-    {
-        _stateMachine.ChangeState(newState);
-    }
+    
+    
 }
